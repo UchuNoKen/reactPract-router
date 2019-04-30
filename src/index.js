8c6752f3ed10577fd376aa98ec8e9f9a6eadf609
UchuNoKen <xen.o.fite@gmail.com>
2019-04-30 07:36:55 -0400

    added routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// components
+import Home from "./components/Home";
+import Posts from "./components/Posts";
+import Profile from "./components/Profile";
 
 const App = () => {
-  return <div>Home</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>Header</h1>
+        <Route exact path="/" component={Home} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </BrowserRouter>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
