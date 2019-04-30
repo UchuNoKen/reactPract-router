a406ed6deaddfa49410ed47ae0ad5b7fae6d684d
UchuNoKen <xen.o.fite@gmail.com>
2019-04-30 14:27:25 -0400

    hoc refactor

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Card from "../hoc/Card";
+import Auth from "../hoc/Auth";
 
 const Profile = props => {
   const redir = () => {
-    props.history.push("/");
+    // props.history.push("/");
   };
 
   return (
-    <div>
-      <Link to={{ pathname: `${props.match.url}/posts` }}>Take me to /profile/posts</Link>
-
-      {redir()}
-    </div>
+    <Auth>
+      <Card>
+        <div>
+          <Link to={{ pathname: `${props.match.url}/posts` }}>Take me to /profile/posts</Link>
+        </div>
+      </Card>
+    </Auth>
   );
 };
 
